95d50b86007540cc0954dfc8b6b23e3a880473f6
Willuww <joelbelloromero@gmail.com>
2024-05-21 15:35:30 -0600

    Agregar archivos de proyecto.

---
+++ Api.Microservice.Autor/Aplicacion/Consulta.cs
@@ -0,0 +1,33 @@
+﻿using Api.Microservice.Autor.Modelo;
+using Api.Microservice.Autor.Persistencia;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Microservice.Autor.Aplicacion
+{
+    public class Consulta
+    {
+        //que va a devolver la clase .. patron ETR
+        public class ListaAutor: IRequest<List<AutorDto>> { }
+
+        //recibe LitaAutor y regresa una lista de AutorLibro
+        public class Manejador : IRequestHandler<ListaAutor, List<AutorDto>>
+        {
+            private readonly ContextoAutor _context;
+            private readonly IMapper _maper;
+            public Manejador(ContextoAutor context, IMapper mapper)
+            {
+                this._context = context;
+                this._maper = mapper;
+            }
+
+            public async Task<List<AutorDto>> Handle(ListaAutor request, CancellationToken cancellationToken)
+            {
+                var autores = await _context.AutorLibros.ToListAsync();
+                var autoresDto = _maper.Map<List<AutorLibro>, List< AutorDto>> (autores);
+                return autoresDto;
+            }
+        }
+    }
+}
